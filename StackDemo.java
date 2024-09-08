c57f95c90b53933c4963367165a42b4971dfc0b9
Parsa Faraji Alamouti <165321600+parsa-faraji@users.noreply.github.com>
2024-09-08 10:50:13 -0700

    Create StackDemo.java
    stacks in java

---
+++ StackDemo.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
