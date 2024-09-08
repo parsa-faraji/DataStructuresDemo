7a4b4c91c9d8f5f68f601ea06760b9ef1e10727f
Parsa Faraji Alamouti <165321600+parsa-faraji@users.noreply.github.com>
2024-09-08 10:51:06 -0700

    Delete ArrayExample.java
    delete

--- ArrayExample.java
+++
@@ -1,9 +0,0 @@
-public class ArrayExample {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-    }
-}
-
